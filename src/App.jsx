348855daaedc6e84b29c71a51481dbe4389a54f7
Firat Can Tas <firatcantass@gmail.com>
2026-07-19 22:09:20 +0200

    feat: add first semantic tokens

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div className="bg-amber-700">New Product</div>;
+  return <div className="bg-bg text-text border-border border p-4">New Product</div>;
 };
 
 export default App;
