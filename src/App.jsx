3caf967e3ce16a4ace52018e9a82f146eaed135d
can.tas <firatcantass@gmail.com>
2026-07-28 14:56:19 +0200

    feat: apply nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Contacts from './pages/Contacts';
 import Deals from './pages/Deals';
 import Activities from './pages/Activities';
 import NotFound from './pages/NotFound';
+import AppShell from './components/layout/AppShell';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/contacts" element={<Contacts />} />
-      <Route path="/deals" element={<Deals />} />
-      <Route path="/activities" element={<Activities />} />
+      <Route path="/" element={<AppShell />}>
+        <Route index element={<Dashboard />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/deals" element={<Deals />} />
+        <Route path="/activities" element={<Activities />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
