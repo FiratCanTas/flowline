92ddf00c32fbc26b9b31b886a56247eb274d4eef
can.tas <firatcantass@gmail.com>
2026-08-03 17:43:29 +0200

    feat: display contacts list from api

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,4 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import { getContacts } from '../features/contacts/api/contacts';
+
 const Contacts = () => {
-  return <>Contacts</>;
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['contacts'],
+    queryFn: getContacts,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  } else if (error) {
+    return <span>{error.message}</span>;
+  }
+
+  return (
+    <ul>
+      {data?.map((contact) => (
+        <li key={contact.id}>{contact.name}</li>
+      ))}
+    </ul>
+  );
 };
 export default Contacts;
