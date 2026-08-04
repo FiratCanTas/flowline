3c77bbbb52eadc5863697dd85b6d189281d34d69
can.tas <firatcantass@gmail.com>
2026-08-04 12:47:36 +0200

    feat: add contact search

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getContacts } from '../features/contacts/api/contacts';
+import { useState } from 'react';
 
 const Contacts = () => {
+  const [search, setSearch] = useState('');
+
   const { isLoading, error, data } = useQuery({
     queryKey: ['contacts'],
     queryFn: getContacts,
@@ -13,12 +16,29 @@ const Contacts = () => {
     return <span>{error.message}</span>;
   }
 
+  const handleSearch = (event) => {
+    const text = event.target.value;
+    setSearch(text);
+  };
+
+  const filteredContacts = data?.filter(
+    (contact) =>
+      !search.trim() ||
+      contact.name.toLowerCase().includes(search.toLowerCase()) ||
+      contact.company.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
-    <ul>
-      {data?.map((contact) => (
-        <li key={contact.id}>{contact.name}</li>
-      ))}
-    </ul>
+    <div>
+      <input type="text" value={search} onChange={handleSearch} className="border" />
+      <ul>
+        {filteredContacts?.map((contact) => (
+          <li key={contact.id}>
+            {contact.name} - {contact.company}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 export default Contacts;
