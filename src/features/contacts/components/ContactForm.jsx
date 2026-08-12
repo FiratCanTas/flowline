a6893c440a2ce2592d0fae21592cc2f47fc1ec2c
can.tas <firatcantass@gmail.com>
2026-08-12 15:35:18 +0200

    feat: create RHF contact form

---
+++ src/features/contacts/components/ContactForm.jsx
@@ -0,0 +1,42 @@
+import { useForm } from 'react-hook-form';
+import Button from '../../../components/ui/Button';
+import Input from '../../../components/ui/Input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import contactSchema from '../schema';
+
+const ContactForm = ({ defaultValues }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(contactSchema),
+    defaultValues: defaultValues,
+  });
+
+  const handleFormSubmit = (data) => {
+    console.log('formData:::', data);
+  };
+  return (
+    <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col gap-4">
+      <Input label="Name" id="name" error={errors?.name?.message} {...register('name')} />
+      <Input label="Email" id="email" error={errors?.email?.message} {...register('email')} />
+      <Input label="Phone" id="phone" error={errors?.phone?.message} {...register('phone')} />
+      <Input
+        label="Company"
+        id="company"
+        error={errors?.company?.message}
+        {...register('company')}
+      />
+      <Input
+        label="Position"
+        id="position"
+        error={errors?.position?.message}
+        {...register('position')}
+      />
+      <Button type="submit">Save</Button>
+    </form>
+  );
+};
+
+export default ContactForm;
