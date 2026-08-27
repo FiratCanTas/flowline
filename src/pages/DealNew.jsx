0377ba113c51839c8336630cb7a49a552dd2ba42
can.tas <firatcantass@gmail.com>
2026-08-27 13:39:15 +0200

    feat: enable deal creation

--- src/pages/DealNew.jsx
+++ src/pages/DealNew.jsx
@@ -1,5 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import DealForm from '../features/deals/components/DealForm';
+import { addDeal } from '../features/deals/api/deals';
+import { useNavigate } from 'react-router';
+
 const DealNew = () => {
-  return <>DealNew</>;
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: addDealMutation, isPending } = useMutation({
+    mutationKey: ['add deal'],
+    mutationFn: (data) => addDeal(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['deals'] });
+      navigate('/deals');
+    },
+  });
+
+  return (
+    <div>
+      <DealForm onSubmit={(data) => addDealMutation(data)} disabled={isPending} />
+    </div>
+  );
 };
 
 export default DealNew;
