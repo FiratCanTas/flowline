888cb79c15f42f80ef41c96012b7ec9cce247aaf
can.tas <firatcantass@gmail.com>
2026-09-26 09:33:32 +0200

    refactor: use primitive select component on deal form

--- src/features/deals/components/DealForm.jsx
+++ src/features/deals/components/DealForm.jsx
@@ -6,6 +6,7 @@ import Button from '../../../components/ui/Button';
 import { useQuery } from '@tanstack/react-query';
 import { getContacts } from '../../contacts/api/contacts';
 import LinkButton from '../../../components/ui/LinkButton';
+import Select from '../../../components/ui/Select';
 
 const DealForm = ({ defaultValues, onSubmit, disabled }) => {
   const {
@@ -23,45 +24,38 @@ const DealForm = ({ defaultValues, onSubmit, disabled }) => {
   });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
       <Input label="Title" id="title" error={errors?.title?.message} {...register('title')} />
-      <div>
-        <label htmlFor="contactSelect">Contacts:</label>
-        <select
-          id="contactSelect"
-          className={`${errors?.contactId?.message ? 'border-danger' : 'border-border'}`}
-          disabled={isLoading || error}
-          {...register('contactId')}
-        >
-          <option value="">
-            {isLoading ? 'Loading...' : error ? error?.message : 'Please choose a contact'}
+
+      <Select
+        label="Contacts"
+        id="contactSelect"
+        error={errors?.contactId?.message}
+        disabled={isLoading || error}
+        {...register('contactId')}
+      >
+        <option value="">
+          {isLoading ? 'Loading...' : error ? error?.message : 'Please choose a contact'}
+        </option>
+        {data?.map((contact) => (
+          <option key={contact.id} value={contact.id}>
+            {contact.name}
           </option>
-          {data?.map((contact) => (
-            <option key={contact.id} value={contact.id}>
-              {contact.name}
-            </option>
-          ))}
-        </select>
-        {errors?.contactId?.message && <p>{errors.contactId.message}</p>}
-      </div>
+        ))}
+      </Select>
+
       <Input label="Value" id="value" error={errors?.value?.message} {...register('value')} />
-      <div>
-        <label htmlFor="stageSelect">Stages:</label>
-        <select
-          id="stageSelect"
-          className={`${errors?.stage?.message ? 'border-danger' : 'border-border'}`}
-          {...register('stage')}
-        >
-          <option value="">Please choose a stage</option>
-          <option value="lead">Lead</option>
-          <option value="proposal">Proposal</option>
-          <option value="qualified">Qualified</option>
-          <option value="negotiation">Negotiation</option>
-          <option value="won">Won</option>
-          <option value="lost">Lost</option>
-        </select>
-        {errors?.stage?.message && <p>{errors.stage.message}</p>}
-      </div>
+
+      <Select label="Stages" id="stageSelect" error={errors?.stage?.message} {...register('stage')}>
+        <option value="">Please choose a stage</option>
+        <option value="lead">Lead</option>
+        <option value="proposal">Proposal</option>
+        <option value="qualified">Qualified</option>
+        <option value="negotiation">Negotiation</option>
+        <option value="won">Won</option>
+        <option value="lost">Lost</option>
+      </Select>
+
       <div className="flex gap-2">
         <LinkButton
           variant="secondary"
